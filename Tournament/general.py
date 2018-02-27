08dd497e1fc59714c12b2acc1ebc64aadbb82596
syh7 <sjoerd0707@live.nl>
2018-02-27 14:13:59 +0100

    Added functionality for blind draw group stage.

--- Tournament/general.py
+++ Tournament/general.py
@@ -1,19 +1,23 @@
 import json
 from pprint import pprint
 from random import shuffle
+from itertools import repeat
+import math
 
 players = []
 placements = []
 categories = []
 tour_type = ""
+stage_type = ""
 
 
 def readFile():
-    global players, categories, tour_type
+    global players, categories, tour_type, stage_type
     data = json.load(open("players.json"))
     players = data[0]["players"]
     categories = data[0]["categories"]
     tour_type = data[0]["type"]
+    stage_type = data[0]["stage"]
     # pprint(players)
     # pprint(categories)
 
@@ -34,9 +38,9 @@ def algorithm():
                 cat_players.append(player)
         if cat_players.__len__() > 0:
             if tour_type == "blind":
-                shuffle(cat_players)
+                cat_players = blindGroups(cat_players)
             elif tour_type == "seed":
-                cat_players = seedBrackets(cat_players)
+                cat_players = seededGroups(cat_players)
             # print("cat_players")
             # pprint(cat_players)
             placements.append(cat_players)
@@ -46,12 +50,43 @@ def sortList(playerlist):
     return playerlist
 
 
-def seedBrackets(playerlist):
-    if playerlist.__len__() % 2 == 1:
-        pass
+def blindGroups(playerlist):
+    global stage_type
+    shuffle(playerlist)
+    groups = []
+    length = playerlist.__len__()
+    nrOfGroups = 0
+    if length % 4 == 0 or length % 4 == 1:
+        nrOfGroups = int(length / 4)
     else:
-        pass
+        nrOfGroups = int(math.ceil(length / 4))
+    return roundRobin(playerlist, nrOfGroups)
+
+
+def seededGroups(playerlist):
+    global stage_type
     return []
+
+
+def roundRobin(playerlist, nrOfGroups):
+    groups = [[] for i in repeat(None, nrOfGroups)]
+    player_iterable = iter(playerlist)
+    currentGroup = 0
+    nextGroup = 1
+    while player_iterable:
+        try:
+            player = next(player_iterable)
+            groups[currentGroup].append(player)
+            currentGroup += nextGroup
+            if currentGroup == nrOfGroups:
+                nextGroup = -1
+                currentGroup -= 1
+            elif currentGroup == -1:
+                nextGroup = 1
+                currentGroup += 1
+        except StopIteration:
+            break
+    return groups
 
 
 readFile()
